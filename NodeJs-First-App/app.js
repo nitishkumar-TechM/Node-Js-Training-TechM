30754bb677fd2e9086eb68cd83aa66784f18a05e
nitishkumar-TechM <nkcm12345@gmail.com>
2025-07-29 11:19:57 +0000

    writeFileSync method

---
+++ NodeJs-First-App/app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {2
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><label>Enter Message: </label><input type="text" name="messageInput"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    // process.exit();
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);  // Synchronous file write (blocking code)
+            // console.log(message);
+            // log(message); // Assuming log function is imported from logger.js
+            res.writeHead(302, { Location: '/' });
+            return res.end();
+        });
+       
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(4004);
+    
